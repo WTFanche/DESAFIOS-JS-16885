dda9a48942687dec7d47150b3ee24f2ffb9ada15
WTFanche <sanchez.facundo.r@gmail.com>
2021-10-21 20:34:58 -0300

    Desafio 1

---
+++ Desafio 1/main.js
@@ -0,0 +1,10 @@
+alert("Desafio 1 - Crear un algoritmo simple");
+
+let nombreUsuario = prompt("Ingresa tu nombre");
+let apellidoUsuario = prompt("Ingesa tu apellido");
+
+console.log("Hola " + nombreUsuario + " " + apellidoUsuario);
+
+alert("Hola " + nombreUsuario + " " + apellidoUsuario)
+
+alert("¡Saludos!")
