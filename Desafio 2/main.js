558083c01c252d3b8af7b9294a5cf0faec9b512e
WTFanche <sanchez.facundo.r@gmail.com>
2021-10-23 15:16:07 -0300

    Desafio 2

---
+++ Desafio 2/main.js
@@ -0,0 +1,31 @@
+alert("Desafio 2 - Crear un algoritmo con un condicional");
+
+let fechaNacimiento = prompt(`¡Bienvenido!
+Ingresá tu año de nacimiento para saber a qué generación pertenecés.`);
+
+if (fechaNacimiento < 1965) {
+    alert("Pertenecés a la generación Baby Boomers.");
+    console.log("Pertenecés a la generación Baby Boomers.");
+} 
+else if ((fechaNacimiento > 1964) && (fechaNacimiento < 1980))
+{
+    alert("Pertenecés a la generación X.");
+    console.log("Pertenecés a la generación X.");
+} 
+else if ((fechaNacimiento > 1979) && (fechaNacimiento < 2000))
+{
+    alert("Pertenecés a la generación Milennials.");
+    console.log("Pertenecés a la generación Milennials.");
+}
+else if (fechaNacimiento > 1999 ) 
+{
+    alert("Pertenecés a la generación Z.");
+    console.log("Pertenecés a la generación Z.");
+}
+else
+{
+    alert("Dato ingresado erróneo.");
+};
+
+alert("¡Gracias por tu visita!");
+console.log("¡Gracias por tu visita!");
